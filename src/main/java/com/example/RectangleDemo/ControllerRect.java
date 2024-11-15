14bd30be315d2975900656f61442b578960aef93
Tatka 10 <ccards@bk.ru>
2024-11-15 20:33:43 +0300

    Прямоугольник сохраняем в список введенные прямоугольники

--- src/main/java/com/example/RectangleDemo/ControllerRect.java
+++ src/main/java/com/example/RectangleDemo/ControllerRect.java
@@ -1,25 +1,30 @@
 package com.example.RectangleDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerRect {
+    //вариант сделать список  - прямо полем контроллера
+    //List<Rectangle> historyList = new ArrayList<>();
+    //вариант современный, по-спринговски, сделать бин и связать через @service с бином контроллера
+    @Autowired
+    RectangleService rectangleService;
+
     @GetMapping("/get_result")
     public String getResultFunc(String a, String b, Model model) throws GeometryException {
         try {
             int x = Integer.parseInt(a);
             int y = Integer.parseInt(b);
-
-
             Rectangle rect = new Rectangle(x, y);
-
-
             double recPerimetr = rect.perimeter();
             double recArea = rect.area();
             model.addAttribute("recPerimetr", rect.perimeter());
             model.addAttribute("recArea", rect.area());
+            //historyList.add(rect);
+            rectangleService.add(rect);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             model.addAttribute("errorMes", "Некорректные данные: " + e.getMessage());
@@ -30,8 +35,15 @@ public class ControllerRect {
         return "result";
     }
 
-    public void saveRectangles(Rectangle rect) {
+    @GetMapping("/history")
+    public String showHistory(Model model) {
+        //model.addAttribute("message", historyList.toString());
+        model.addAttribute("message", rectangleService.getRectangleList().toString());
 
 
+        return "history";
     }
+
+
 }
+
---
+++ src/main/java/com/example/RectangleDemo/RectangleService.java
@@ -0,0 +1,17 @@
+package com.example.RectangleDemo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class RectangleService {
+    List<Rectangle> rectangleList= new ArrayList<>();
+    public void add (Rectangle rect){
+        rectangleList.add(rect);
+    }
+
+    public List<Rectangle> getRectangleList() {
+        return rectangleList;
+    }
+}
